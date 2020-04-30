022c7815c95be13d0335c086ff728c17b74cf1f9
ChristopherColton <50006085+ChristopherColton@users.noreply.github.com>
2020-04-30 14:45:09 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,55 +1,62 @@
-/** 
-function draw() {
-    if (mouseIsPressed) {
-        fill(0);
-    } else {
-        fill(255);
-    }
-    text("A", mouseX, mouseY);
-}*/
+var circles = [];
+var probability = 0.25;
 
-var circ1;
-var circ2;
+// Constants for viral states
+const SUSCEPTIBLE = 0;
+const INFECTED = 1;
+const REMOVED = 2;
 
 function setup() { 
   createCanvas(400, 200);
-  circ1 = new Circle();
-  circ2 = new Circle();
+  for (let i = 0; i < 10; i++) {
+    circles.push(new Circle());
+  }
   ellipseMode(CENTER);
 } 
 
 function draw() { 
   background(255);
-  
-  circ1.display();
-  circ2.display();
-  
-  
-  var d = dist(circ1.x1, circ1.y1, circ2.x2, circ2.y2);
-  
-  if (d < circ1.r + circ2.r) {
-    //fill(150);
-    //fill(changeColor());
-    circ1.changeColor();
-  	circ2.changeColor();
+
+  update();
+
+  for (let i = 0; i < circles.length; i++) {
+    circles[i].display();
   }
 }
 
-function Circle() {
-  this.x1 = 50;
-  this.y1 = 100;
-  this.x2 = 75;
-  this.y2 = 100;
+function update() {
+  for (let i = 0; i < circles.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (i.isTouching(j)) {
+        if (j.state == INFECTED && random(0, 1) < probability) {
+          i.state = INFECTED;
+        }
+        if (i.state == INFECTED && random(0, 1) < probability) {
+          j.state = INFECTED;
+        }
+      }
+    }
+  }  
+}
+
+function Circle(x, y) {
+  this.x = x;
+  this.y = y;
+  this.state = SUSCEPTIBLE;
   this.r = 25;
   this.col = color(255);
   
   this.display = function() {
-    ellipse(this.x1, this.y1, this.r*2);
-    ellipse(this.x2, this.y2, this.r*2);
+    console.log("Displaying circle at " + x + ", " + y);
     fill(this.col);
+    ellipse(this.x, this.y, this.r*2);
   }
   
   this.changeColor = function() {
     this.col = color('red');
+  }
+
+  this.isTouching = function(other) {
+    return dist(this.x, this.y, other.x, other.y) < this.r + other.r;
   }
 }
