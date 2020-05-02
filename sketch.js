4f87782ed00c1127b3d3da92afef2400f97314ab
ChristopherColton <50006085+ChristopherColton@users.noreply.github.com>
2020-05-02 15:08:16 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,56 @@
 var circles = [];
 var probability = 0.25;
+var x = 50;
+var y = 50;
+var stateColors;
+var horizSpacing = 100;
+var vertSpacing = 100;
+var leftMargin = 0;
+var rightMargin = 1000;
+
+//pictures
+var clarinet;
+var drum;
+var sax;
+var tuba;
+var violin;
+var instrumentImages;
 
 // Constants for viral states
 const SUSCEPTIBLE = 0;
 const INFECTED = 1;
 const REMOVED = 2;
 
-function setup() { 
-  createCanvas(400, 200);
-  for (let i = 0; i < 10; i++) {
-    circles.push(new Circle());
+function preload(){
+  clarinet = loadImage('images/Clarinet1.png');
+  drum = loadImage('images/Drum1.png');
+  sax = loadImage('images/Sax1.png');
+  tuba = loadImage('images/Tuba1.png');
+  violin = loadImage('images/Violin1.png');
+}
+
+function setup() {
+  stateColors = [
+    color('white'),
+    color('red'),
+    color('green')
+  ];
+  instrumentImages = [
+    clarinet, drum, sax, tuba, violin
+  ];
+  createCanvas(1000, 600);
+  for (let i = leftMargin; i < 10; i++) {
+    circles.push(new Circle(x, y));
+    x += horizSpacing;
+    if (x > rightMargin)
+    {
+      x = leftMargin;
+      y += vertSpacing;
+    }
   }
   ellipseMode(CENTER);
+  imageMode(CENTER);
+  circles[0].state = INFECTED;
 } 
 
 function draw() { 
@@ -19,6 +58,9 @@ function draw() {
 
   update();
 
+  for (let i = 0; i < circles.length; i++) {
+    circles[i].displayRadius();
+  }
   for (let i = 0; i < circles.length; i++) {
     circles[i].display();
   }
@@ -30,6 +72,7 @@ function update() {
     for (let j = 0; j < i; j++) {
       let circ2 = circles[j];
       if (circ1.isTouching(circ2)) {
+         
         if (circ2.state == INFECTED && random(0, 1) < probability) {
           circ1.state = INFECTED;
         }
@@ -45,20 +88,26 @@ function Circle(x, y) {
   this.x = x;
   this.y = y;
   this.state = SUSCEPTIBLE;
-  this.r = 25;
-  this.col = color(255);
+  this.r = 100;
+  this.instrument = floor(random(5));
   
-  this.display = function() {
-    console.log("Displaying circle at " + x + ", " + y);
-    fill(this.col);
-    ellipse(this.x, this.y, this.r*2);
-  }
-  
-  this.changeColor = function() {
-    this.col = color('red');
-  }
 
+  // Display radius
+  this.displayRadius = function() {
+    noStroke();
+    fill(color('pink'));
+    ellipse(this.x, this.y, this.r*2);
+  };
+  
+  // Display actual instrument
+  this.display = function() {
+    stroke(color('black'));
+    fill(stateColors[this.state]);
+    ellipse(this.x, this.y, this.r);
+    image(instrumentImages[this.instrument], this.x, this.y, this.r, this.r);
+  };
+  
   this.isTouching = function(other) {
     return dist(this.x, this.y, other.x, other.y) < this.r + other.r;
-  }
+  };
 }
