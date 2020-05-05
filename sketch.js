514299d266276504e0c0a0ebc63c18f9f74a6678
ChristopherColton <50006085+ChristopherColton@users.noreply.github.com>
2020-05-04 18:36:25 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,6 +3,8 @@ var circles = [];
 
 // Chance of getting infected
 var probability = 0.25;
+var reinfect = false;
+var radius = 100;
 
 // X and y of... what again?
 var x = 50;
@@ -59,7 +61,7 @@ function setup() {
   instrumentTracks = [
     instOne, instTwo, instThree
   ];
-  createCanvas(1000, 600);
+  canvas = createCanvas(1000, 600);
   for (let i = leftMargin; i < 10; i++) {
     circles.push(new Circle(x, y));
     x += horizSpacing;
@@ -69,26 +71,39 @@ function setup() {
       y += vertSpacing;
     }
   }
+  createElement('br');
   //button to start
   buttonStart = createButton('Start');
-  buttonStart.position(100,100);
+  //buttonStart.position(-500,100, 'sticky');
   buttonStart.mousePressed(play);
   //button to stop
   buttonStop = createButton('Stop');
-  buttonStop.position(150,100);
+  //buttonStop.position(-500,150, 'sticky');
   buttonStop.mousePressed(stop);
   //button to reset simulation
   buttonReset = createButton('Reset');
-  buttonReset.position(200,100);
+  //buttonReset.position(-500,200, 'sticky');
   buttonReset.mousePressed(reset);
   //slider probability
+  createP("Infection probability: ");
   sliderProb = createSlider(0,100,25);
-  sliderProb.position(100,50);
+  //sliderProb.position(100,50);
   sliderProb.style('width', '100px');
+  //slider for orchestra size
+  createP("Orchestra size: ");
+  sliderOSize = createSlider(0,100,25);
+  //sliderOSize.position(100,75);
+  //slider for radius size
+  createP("Infection radius: ");
+  sliderRSize = createSlider(0,100,25);
+  //sliderRSize.position(100,100);
+  //check for reinfection or not
+  checkbox = createCheckbox('Reinfection', false);
 
   ellipseMode(CENTER);
   imageMode(CENTER);
   circles[0].state = INFECTED;
+  circles[0].infectedCountdown = floor(random(2, 5));
 } 
 
 function play() {
@@ -118,15 +133,24 @@ function stop() {
 }
 
 
-function draw() { 
+
+function draw() {
+
+  // Draw background
   background(255);
 
+  // Take slider and checkbox inputs
   probability = sliderProb.value() / 100;
+  reinfect = checkbox.checked();
+  radius = sliderRSize.value();
 
+  
+  // Calculate generation
   if (tick % 100 == 0 && playing) {
     update();
   }
 
+  // Display circles
   for (let i = 0; i < circles.length; i++) {
     circles[i].displayRadius();
   }
@@ -134,27 +158,61 @@ function draw() {
     circles[i].display();
   }
 
+  // Increase tick
   if (playing) tick++;
+
+  // Draw text
   fill(color('red'));
   textSize(50);
   text("Day " + floor(tick/100), 0, 400);
+  textSize(25);
+  text("Susceptible: " + countState(SUSCEPTIBLE), 0, 450);
+  text("Infected: " + countState(INFECTED), 0, 500);
+  
+  //text for sliders
+  text('Infection Probability',sliderProb.x * 2 + sliderProb.width, 35);
+  text('Orchestra Size');
+  text('Radius Size');
+  
 }
 
 function update() {
+  // Start with copy of previous generation
+  for (let circ of circles) {
+    circ.newState = circ.state;
+  }
+  // Count down days until each infected instrument is removed
+  for (let circ of circles) {
+    if (circ.state == INFECTED) {
+      circ.infectedCountdown--;
+      if (circ.infectedCountdown < 1) {
+        circ.newState = REMOVED;
+      }
+    }
+  }
+  // Iterate through every pair of circles, possibly infecting if they touch
   for (let i = 0; i < circles.length; i++) {
     let circ1 = circles[i];
     for (let j = 0; j < i; j++) {
       let circ2 = circles[j];
       if (circ1.isTouching(circ2)) {
          
-        if (circ2.state == INFECTED && random(0, 1) < probability) {
-          circ1.state = INFECTED;
+        if (circ2.state == INFECTED && random(0, 1) < probability && circ1.infectable()) {
+          circ1.newState = INFECTED;
+          circ1.infectedCountdown = floor(random(2, 5));
         }
-        if (circ1.state == INFECTED && random(0, 1) < probability) {
-          circ2.state = INFECTED;
+        
+        if (circ1.state == INFECTED && random(0, 1) < probability && circ2.infectable()) {
+          circ2.newState = INFECTED;
+          circ2.infectedCountdown = floor(random(2, 5));
         }
+
       }
     }
+  }
+  // Set state to newly created state
+  for (let circ of circles) {
+    circ.state = circ.newState;
   }
 }
 
@@ -162,26 +220,60 @@ function Circle(x, y) {
   this.x = x;
   this.y = y;
   this.state = SUSCEPTIBLE;
-  this.r = 100;
   this.instrument = floor(random(5));
+  this.infectedCountdown = 0;
   
 
   // Display radius
   this.displayRadius = function() {
     noStroke();
     fill(color('pink'));
-    ellipse(this.x, this.y, this.r*2);
+    ellipse(this.x, this.y, radius*2);
   };
   
   // Display actual instrument
   this.display = function() {
     stroke(color('black'));
     fill(stateColors[this.state]);
-    ellipse(this.x, this.y, this.r);
-    image(instrumentImages[this.instrument], this.x, this.y, this.r, this.r);
+    ellipse(this.x, this.y, radius);
+    image(instrumentImages[this.instrument], this.x, this.y, radius, radius);
   };
   
   this.isTouching = function(other) {
-    return dist(this.x, this.y, other.x, other.y) < this.r + other.r;
+    return dist(this.x, this.y, other.x, other.y) < radius + radius;
   };
+
+  this.infectable = function() {
+    return this.state == SUSCEPTIBLE || (reinfect && this.state == REMOVED);
+  }
+}
+
+function countInstruments(instNumber) {
+  let n = 0;
+  for (let circ of circles) {
+    if (circ.instrument == instNumber) {
+      n++;
+    }
+  }
+  return n;
+}
+
+function countInstrumentState(instNumber, state) {
+  let n = 0;
+  for (let circ of circles) {
+    if (circ.instrument == instNumber && circ.state == state) {
+      n++;
+    }
+  }
+  return n;
+}
+
+function countState(state) {
+  let n = 0;
+  for (let circ of circles) {
+    if (circ.state == state) {
+      n++;
+    }
+  }
+  return n;
 }
